ec02ebb3a4e8e68e28da3d996febf80b4d70ace6
Dan Whettam <qc19291@it077305.wks.bris.ac.uk>
2021-09-22 12:13:55 +0100

    added optimizer, criterion etc. correct time axis desctruction to match paper

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import time
 
 import utils
 
+import torch
 import torchaudio
+import torch.nn as nn
 from torch.utils.data import DataLoader
 
 from DCASE_Dataset import DCASE_Dataset
+from model import DCASEModel
 
 
 parser = argparse.ArgumentParser(description='DCASE CNN')
@@ -14,18 +17,38 @@ parser.add_argument('data', metavar='DIR',
                     help='path to dataset')
 parser.add_argument('epochs', type=int, default=10,
                     help='number of epochs')
+parser.add_argument('-p', '--print_freq', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('-e', '--eval_freq', default=1, type=int,
+                    metavar='N', help='print frequency (default: 1)')
 args = parser.parse_args()
 
 torchaudio.set_audio_backend("sox_io")
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 def main():
     dataset = DCASE_Dataset(args.data)
     dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
+    _, _, freq_dim, time_dim = next(iter(dataloader)).size()
+
+    model = DCASEModel(freq_dim, time_dim)
+    if torch.cuda.device_count() > 1:
+        print("Let's use", torch.cuda.device_count(), "GPUs!")
+        model = nn.DataParallel(model)
+
+    model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     for epoch in args.epochs:
-        train(dataloader)
+        run_phase(dataloader, criterion, optimizer, epoch, args, phase='train')
 
-def train(loader, model, criterion, optimizer, epoch, args):
+        if epoch % args.eval_freq == 0:
+            run_phase(dataloader, criterion, optimizer, epoch, args, phase='val')
+
+
+def run_phase(loader, model, criterion, optimizer, epoch, args, phase='train'):
     batch_time = utils.AverageMeter('Time', ':6.3f')
     data_time = utils.AverageMeter('Data', ':6.3f')
     losses = utils.AverageMeter('Loss', ':.4e')
@@ -34,14 +57,37 @@ def train(loader, model, criterion, optimizer, epoch, args):
     progress = utils.ProgressMeter(
         len(loader),
         [batch_time, data_time, losses, top1, top5],
-        prefix="Epoch: [{}]".format(epoch))
+        prefix="Epoch: [{}] | {} | ".format(epoch, phase))
 
     # switch to train mode
-    model.train()
+    model.train(phase == 'train')
 
     end = time.time()
-    for i, (images, target) in enumerate(dataloader):
+    for i, (specs, target) in enumerate(loader):
         data_time.update(time.time() - end)
+        specs = specs.to(device)
+        target = target.to(device)
+
+        output = model(specs)
+        loss = criterion(output, target)
+
+        acc1, acc5 = utils.accuracy(output, target, topk=(1, 5))
+        losses.update(loss.item(), specs.size(0))
+        top1.update(acc1[0], specs.size(0))
+        top5.update(acc5[0], specs.size(0))
+
+        if phase == 'train':
+            # compute gradient and do SGD step
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            progress.display(i)
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -5,17 +5,18 @@ from typing import Any
 
 class DCASEModel(nn.Module):
 
-    def __init__(self) -> None:
+    def __init__(self, freq_dim, time_dim) -> None:
         super(DCASEModel, self).__init__()
 
+        freq_dim_qtr = freq_dim/ 4
         self.layer1 = nn.sequential(nn.Conv2d(1, 128, kernel_size=(5, 5), stride=1, padding=0),
                                     nn.ReLu(),
-                                    nn.MaxPool2d(5, 5),
+                                    nn.MaxPool2d((5, 5)),
                                     )
 
         self.layer2 = nn.sequential(nn.Conv2d(128, 256, kernel_size=(5, 5), stride=1, padding=0),
                                     nn.ReLu(),
-                                    TimeDestructMaxPool(5),
+                                    nn.MaxPool2d((freq_dim_qtr, time_dim)),
                                     )
 
         self.fc_layer = nn.Sequential(nn.Linear(256, 15),
@@ -27,22 +28,3 @@ class DCASEModel(nn.Module):
         x = self.fc_layer(x)
         return x
 
-
-class TimeDestructMaxPool(nn.Module):
-
-    def __init__(self, kernel_size) -> None:
-        super(TimeDestructMaxPool, self).__init__()
-
-        self.temporal_max_pool = nn.MaxPool1d(kernel_size)
-        self.freq_max_pool = nn.MaxPool1d(kernel_size)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        x = self.temporal_max_pool(x)
-
-        freq_dim_qtr = x.size()[2] / 4
-        x = x.permute(0, 1, 3, 2)
-        for idx in range(1, 4):
-            dims = idx * freq_dim_qtr
-            x = self.freq_max_pool(x[:, :, :, (dims - freq_dim_qtr):dims])
-        x = x.permute(0, 1, 3, 2)
-        return x
